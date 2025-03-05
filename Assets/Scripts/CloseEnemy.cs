3029e21d62cc534de12548abf01c194c5b1ff68a
Mira Melendez <mmelendez1107@gmail.com>
2025-03-05 12:37:09 -0500

    mira's enemy ai
    piece of a state machine i tried but had issues with layer meshes and stuff. doesn't work 😭

--- Assets/Scripts/CloseEnemy.cs
+++ Assets/Scripts/CloseEnemy.cs
@@ -1,33 +1,132 @@
-using System;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class CloseEnemy : MonoBehaviour, EnemyInterface
 {
     [SerializeField]
-    public int HP {get; set;} = 100;
+    public int HP { get; set; } = 100;
     public Transform player;
     public NavMeshAgent agent;
+    public LayerMask whatIsGround, whatIsPlayer;
 
+    //Patrolling
+    public Vector3 walkPoint;
+    bool walkPointSet = false;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    void Awake()
+    {
+        player = GameObject.Find("TempPlayer").transform;
+        agent = GetComponent<NavMeshAgent>();
+        
+    }
+    void Update()
+    {
+        //check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange)
+        {
+            Patrol();
+        }
+
+        if (playerInSightRange && !playerInAttackRange)
+        {
+            Aggro();
+        }
+
+        if (playerInSightRange && playerInAttackRange)
+        {
+            Attack();
+        }
+    }
     public void TakeDamage(int amount)
     {
         Debug.Log(amount);
         HP -= amount;
         Debug.Log(HP);
         Debug.Log(amount);
-        if(HP <= 0)
+        if (HP <= 0)
         {
             Destroy(this.gameObject);
         }
-    }  
-
-    void Awake()
-    {
-        agent = GetComponent<NavMeshAgent>();
     }
 
-    void Update()
+    public void Patrol()
     {
-        agent.destination = player.position;
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distance = transform.position - walkPoint;
+
+        //walkpoint reached
+        if (distance.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
     }
+
+    public void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = UnityEngine.Random.Range(-walkPointRange, walkPointRange);
+        float randomx = UnityEngine.Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomx, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    public void Aggro()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    public void Attack()
+    {
+        //make sure enemy doesn't move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            //mele attack code here!!!
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
+    public void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+
 }
